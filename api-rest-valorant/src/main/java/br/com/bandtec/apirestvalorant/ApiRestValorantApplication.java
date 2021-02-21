81def23d321fa45c24414878958dc33e47e588ea
jorge-lsb <jorge.barbosa@bandtec.com.br>
2021-02-21 12:34:58 -0300

    base-project-spring-boot

---
+++ api-rest-valorant/src/main/java/br/com/bandtec/apirestvalorant/ApiRestValorantApplication.java
@@ -0,0 +1,13 @@
+package br.com.bandtec.apirestvalorant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestValorantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestValorantApplication.class, args);
+	}
+
+}
---
+++ api-rest-valorant/src/test/java/br/com/bandtec/apirestvalorant/ApiRestValorantApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bandtec.apirestvalorant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestValorantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
