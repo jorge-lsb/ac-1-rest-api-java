10126875218589788468b20335fe8c4e27c27991
jorge-lsb <jorge.barbosa@bandtec.com.br>
2021-02-21 13:51:11 -0300

    base-project-spotify

---
+++ api-rest-spotify/src/main/java/br/com/bandtec/apirestspotify/ApiRestSpotifyApplication.java
@@ -0,0 +1,13 @@
+package br.com.bandtec.apirestspotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSpotifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestSpotifyApplication.class, args);
+	}
+
+}
---
+++ api-rest-spotify/src/test/java/br/com/bandtec/apirestspotify/ApiRestSpotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bandtec.apirestspotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestSpotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
